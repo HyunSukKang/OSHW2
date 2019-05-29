edcd0ccb9b865a7d4c8edd5b8a6da910735254c3
HyunSukKang <21400018@handong.edu>
2019-05-29 22:57:37 +0900

    OSHW2

--- Problem3_calsum.c
+++ Problem3_calsum.c
@@ -5,9 +5,9 @@
 #include <unistd.h>
  
 #define NUM_THREADS 2
-#define N 1e4
+#define N 10000
 long long sum = 0;
-
+long long subsum[NUM_THREADS];
 
 // matrix multiplication: c = a * b;
 void calsum()
@@ -19,8 +19,6 @@ void calsum()
     usleep(100);
   }
 }
-
-
 // Thread function
 // Each thread calculates the summation over (N / NUM_THREADS) numbers
 // For example, if N = 10 and NUM_THREADS = 2
@@ -28,10 +26,18 @@ void calsum()
 // and thread 1 calcualtes the sum of [6 7 8 9 10]
 void* thread_function(void* argument) {
 // ****** Implement this function *******
+  long long token = N / NUM_THREADS;
+  // printf("token : %lld\n", token);
+  long long  th_num = *((int*) argument);
+  printf("thread %lld : add %lld to %lld\n", th_num, (th_num*token)+1, (th_num+1)*token);
+  long long  j = (th_num * token) + 1;
+  subsum[th_num] = 0;
+  for(; j<=(th_num+1) * token; j++){
+    subsum[th_num] += j;
+    usleep(100);
+  }
+  sum += subsum[th_num];
 
-
-
-  
   return NULL;
 }
 
@@ -39,9 +45,25 @@ void* thread_function(void* argument) {
 void calsum_MultiThread()
 {
  // ****** Implement this function *******
+  pthread_t threads[NUM_THREADS];
+  int init_value[NUM_THREADS];
+  int thr_id;
+  int result_code;
+  unsigned index;
 
+  for (index = 0; index < NUM_THREADS; index++) {
+    init_value[index] = index;
 
+    // Creating a thread with parameters
+    thr_id = pthread_create(&threads[index], NULL, thread_function, &init_value[index]);
+    assert(!thr_id);
+  }
 
+  for (index = 0; index < NUM_THREADS; index++) {
+    // block until ith thread completes
+    result_code = pthread_join(threads[index], NULL);
+    assert(!result_code); // if error_code (non-zero value) returned than terminate.
+  }
 
 }
 
@@ -52,21 +74,23 @@ int main(int argc, char** argv) {
 
   // single thread
   start_t = clock();
-  calsum();sum_s = sum;
+  calsum();
+  sum_s = sum;
   end_t = clock();
   uSec_s = 1000000*(float)(end_t - start_t)/CLOCKS_PER_SEC;
   
-
   sum = 0;
+
   // multi thread
- // start_t = clock();
- // calsum_MultiThread();sum_m = sum;
- // end_t = clock();
- // uSec_m = 1000000*(float)(end_t - start_t)/CLOCKS_PER_SEC;
+  start_t = clock();
+  calsum_MultiThread();
+  sum_m = sum;
+  end_t = clock();
+  uSec_m = 1000000*(float)(end_t - start_t)/CLOCKS_PER_SEC;
   
 
   printf("Single: sum = %lld\t%f\n", sum_s,uSec_s);
- // printf("Multip: sum = %lld\t%f\n", sum_m,uSec_m);
+  printf("Multip: sum = %lld\t%f\n", sum_m,uSec_m);
 }
 
 
